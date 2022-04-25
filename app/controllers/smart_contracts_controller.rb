0dc431d8e0647c6c7631d8a37b1b9d15fe282899
Ryan Kulp <ryanckulp@gmail.com>
2022-04-26 03:53:16 +0900

    simplify smart contract CRUD

--- app/controllers/smart_contracts_controller.rb
+++ app/controllers/smart_contracts_controller.rb
@@ -12,7 +12,6 @@ class SmartContractsController < ApplicationController
   # user makes sure everything is OK, then clicks to deploy client-side and pays gas
   def show
     @smart_contract = SmartContract.find_by(user_id: current_user.id, id: params[:id])
-    setup_deployment
   end
 
   def update
@@ -31,29 +30,6 @@ class SmartContractsController < ApplicationController
 
   def smart_contract_params
     params.require(:smart_contract).permit(:name, :symbol, :creator_wallet_address, :network, :mint_qty_max, :purchase_price_friendly)
-  end
-
-  # compiles contract with Ruby https://github.com/q9f/eth.rb/#28-solidity-compiler-bindings
-  # TODO: move to service object, rename tmp files
-  def setup_deployment
-    file = File.read("#{Rails.root}/MintSite.sol")
-
-    file.gsub!('MINT_PRICE = 1000000000000000000', "MINT_PRICE = #{@smart_contract.purchase_price.round(0)}")
-    file.gsub!('MAX_PURCHASE_LIMIT = 10', "MAX_PURCHASE_LIMIT = #{@smart_contract.mint_qty_max}")
-    file.gsub!('payable(0x0000000000000000000000000000000000000000)', "payable(#{@smart_contract.creator_wallet_address})");
-    file.gsub!('MintSiteContractName', @smart_contract.name);
-    file.gsub!('MintSiteContractSymbol', @smart_contract.symbol);
-
-    File.open("MintSiteStaging.sol", "w") { |f| f.write file }
-
-    solc = Eth::Solidity.new
-    raw_contract = solc.compile 'MintSiteStaging.sol'
-
-    contract = raw_contract[@smart_contract.name]
-    @abi = contract['abi']
-    @bytes = contract['bin']
-
-    @smart_contract.update(abi: @abi)
   end
 
   def set_smart_contract
