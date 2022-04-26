4eb63bf33150b63d88b2996dd0f3737cad9ba574
Ryan Kulp <ryanckulp@gmail.com>
2022-04-26 17:56:45 +0900

    helpers to verify and check status of a deployed polygon contract on external server

--- app/models/concerns/contractable.rb
+++ app/models/concerns/contractable.rb
@@ -29,12 +29,41 @@ module Contractable
     self.purchase_price = num_str.to_f
   end
 
+  def solidity_server_base_uri
+    'http://4x5v.hatchboxapp.com'
+  end
+
   def compile!
-    url = 'http://4x5v.hatchboxapp.com/contract_compilations'
+    url = solidity_server_base_uri + '/contract_compilations'
     params = { name: name, purchase_price: purchase_price.round(0), mint_qty_max: mint_qty_max, creator_wallet_address: creator_wallet_address, symbol: symbol }
     resp = HTTParty.post(url, body: params)
     data = JSON.parse(resp.body)
 
-    update(abi: data['abi'], bytecode: data['bytes'])
+    update(abi: data['abi'], bytecode: data['bytes'], source_code: data['source'])
+  end
+
+  def verify_and_check!
+    verify!
+    check!
+  end
+
+  def verify!
+    url = solidity_server_base_uri + '/contract_verifications'
+    params = attributes.slice('address', 'source_code', 'name', 'network')
+
+    resp = HTTParty.post(url, body: params)
+    data = JSON.parse(resp.body)
+
+    update(verification_guid: data['result'])
+  end
+
+  def check!
+    url = solidity_server_base_uri + '/contract_verifications?check=true'
+    params = { network: network, guid: verification_guid }
+
+    resp = HTTParty.post(url, body: params)
+    data = JSON.parse(resp.body)
+
+    update(verified: data['verified'])
   end
 end
